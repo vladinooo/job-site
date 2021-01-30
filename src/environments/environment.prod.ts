f44c8e6f89dc38fc081cf85c797c05ac89002ac6
vladinooo <vhartmann@highwirepress.com>
2021-01-30 21:41:05 +0000

    No no!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,7 @@ export const environment = {
     name: 'prod',
     firebase: {
         config: {
-            apiKey: 'AIzaSyAltKZpKrrAoIsUtKPbL0nfrKCn_o8W9NU',
-            authDomain: 'job-site-cac55.firebaseapp.com',
-            projectId: 'job-site-cac55',
-            storageBucket: 'job-site-cac55.appspot.com',
-            messagingSenderId: '247540238632',
-            appId: '1:247540238632:web:8350df03d9fa7e2708281d'
+...
         },
         actionCodeSettings: {
             url: 'http://localhost:5200/profile/new',
